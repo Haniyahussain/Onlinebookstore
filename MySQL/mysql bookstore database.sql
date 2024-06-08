8dc73d74d431727aaac76b573f53fb7e981bd5fd
Haniyahussain <126608303+Haniyahussain@users.noreply.github.com>
2024-06-09 02:07:10 +0500

    Add files via upload

---
+++ MySQL/mysql bookstore database.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE BOOKSTORE;
+USE BOOKSTORE;
+
+-- ----------TABLE FOR BOOKS---------- 
+
+CREATE TABLE BOOKS ( 
+ bookID int primary key
+);
+
+-- ----------TABLE FOR AUTHORS--------- 
+
+CREATE TABLE AUTHORS(
+);
+
+
+-- ----------TABLE FOR PUBLISHERS----------
+
+CREATE TABLE PUBLISHERS(
+);
+
+-- ----------TABLE FOR QUANTITYOFBOOKS-----------
+
+CREATE TABLE QUANTITYOFBOOKS(
+);
+
+
